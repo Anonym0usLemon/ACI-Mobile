1d8592c569999e4aaa2935e4388af9203be29580
Anonym0usLemon <76603698+Anonym0usLemon@users.noreply.github.com>
2024-07-24 11:45:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aci Mobile</title>
+  <link rel="stylesheet" href="./styles/app.css">
+  <link rel="manifest" href="./manifest.json">
+</head>
+
+<body>
+  <div class="window">
+    <header>
+      <img src="./img/banner.jpg" alt="">
+    </header>
+
+    <section class="contact-info">
+      <div class="option">
+        <div></div>
+        <p>Get in Touch!</p>
+        <div></div>
+      </div>
+      <div class="contact-bar">
+        <a href="tel:+18007282722"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path
+              d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm90.7 96.7c9.7-2.6 19.9 2.3 23.7 11.6l20 48c3.4 8.2 1 17.6-5.8 23.2L168 231.7c16.6 35.2 45.1 63.7 80.3 80.3l20.2-24.7c5.6-6.8 15-9.2 23.2-5.8l48 20c9.3 3.9 14.2 14 11.6 23.7l-12 44C336.9 378 329 384 320 384C196.3 384 96 283.7 96 160c0-9 6-16.9 14.7-19.3l44-12z" />
+          </svg></a>
+        <a href="mailto:cs@acilab.com"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path
+              d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z" />
+          </svg></a>
+        <a href="https://acilab.com"><img src="./img/logo.jpg" alt=""></a>
+        <a
+          href="//maps.google.com/maps?f=q&q=715+East+18th+St,Cedar+Falls,IA 50613,United+States&hnear=715+East+18th+St,Cedar+Falls,IA 50613,United+States&z=15&hl=en"><svg
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <path
+              d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+          </svg></a>
+        <a href="sms:+18007282722"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+            <path
+              d="M88.2 309.1c9.8-18.3 6.8-40.8-7.5-55.8C59.4 230.9 48 204 48 176c0-63.5 63.8-128 160-128s160 64.5 160 128s-63.8 128-160 128c-13.1 0-25.8-1.3-37.8-3.6c-10.4-2-21.2-.6-30.7 4.2c-4.1 2.1-8.3 4.1-12.6 6c-16 7.2-32.9 13.5-49.9 18c2.8-4.6 5.4-9.1 7.9-13.6c1.1-1.9 2.2-3.9 3.2-5.9zM208 352c114.9 0 208-78.8 208-176S322.9 0 208 0S0 78.8 0 176c0 41.8 17.2 80.1 45.9 110.3c-.9 1.7-1.9 3.5-2.8 5.1c-10.3 18.4-22.3 36.5-36.6 52.1c-6.6 7-8.3 17.2-4.6 25.9C5.8 378.3 14.4 384 24 384c43 0 86.5-13.3 122.7-29.7c4.8-2.2 9.6-4.5 14.2-6.8c15.1 3 30.9 4.5 47.1 4.5zM432 480c16.2 0 31.9-1.6 47.1-4.5c4.6 2.3 9.4 4.6 14.2 6.8C529.5 498.7 573 512 616 512c9.6 0 18.2-5.7 22-14.5c3.8-8.8 2-19-4.6-25.9c-14.2-15.6-26.2-33.7-36.6-52.1c-.9-1.7-1.9-3.4-2.8-5.1C622.8 384.1 640 345.8 640 304c0-94.4-87.9-171.5-198.2-175.8c4.1 15.2 6.2 31.2 6.2 47.8l0 .6c87.2 6.7 144 67.5 144 127.4c0 28-11.4 54.9-32.7 77.2c-14.3 15-17.3 37.6-7.5 55.8c1.1 2 2.2 4 3.2 5.9c2.5 4.5 5.2 9 7.9 13.6c-17-4.5-33.9-10.7-49.9-18c-4.3-1.9-8.5-3.9-12.6-6c-9.5-4.8-20.3-6.2-30.7-4.2c-12.1 2.4-24.8 3.6-37.8 3.6c-61.7 0-110-26.5-136.8-62.3c-16 5.4-32.8 9.4-50 11.8C279 439.8 350 480 432 480z" />
+          </svg></a>
+      </div>
+    </section>
+
+    <section class="links">
+      <a class="option" href="https://acilab.com/specials/">
+        <img src="./img/label_sale_gray.png" alt="">
+        <p>ACI & BGT Current Specials</p>
+        <svg class="point-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+          <path
+            d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
+        </svg>
+      </a>
+
+      <a class="option" href="https://acilab.com/my_account_/">
+        <img src="./img/clock_v2_gray.png" alt="">
+        <p>Check the Status of Your Orders!</p>
+        <div></div>
+      </a>
+
+      <a class="option" href="https://www.roesweb.com/configs/ACIRW/products/ACI-Catalog">
+        <img src="./img/cart_v1_gray.png" alt="">
+        <p>Order NOW at ACI Web Prints!</p>
+        <svg class="point-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+          <path
+            d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
+        </svg>
+      </a>
+
+      <a class="option" href="https://acilab.com/email-opt-in/">
+        <img src="./img/email_v3_gray.png" alt="">
+        <p>Sign up for Our Newsletter</p>
+        <svg class="point-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+          <path
+            d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
+        </svg>
+      </a>
+
+      <a class="option" href="https://acilab.com/info/">
+        <img src="./img/products_v6_gray.png" alt="">
+        <p>Request Info on Products & Services</p>
+        <svg class="point-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+          <path
+            d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
+        </svg>
+      </a>
+
+      <a class="option" href="https://help.acilab.com/hc/en-us">
+        <img src="./img/desktop_c1_gray.png" alt="">
+        <p style="padding-right: 8px">Visit our ACI Help Center</p>
+        <svg class="point-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+          <path
+            d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
+        </svg>
+      </a>
+
+      <a class="option"
+        href="https://www.google.com/maps/place//data=!4m3!3m2!1s0x87e5553428b54a1d:0x86de7b92b457d099!12e1?source=g.page.m.dd._&laa=lu-desktop-reviews-dialog-review-solicitation">
+        <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
+        </svg>
+        <p style="padding-right: 8px">Share Why You Love ACI!</p>
+        <svg class="point-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+          <path
+            d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
+        </svg>
+      </a>
+
+      <div class="option">
+        <div style="padding-inline: 26px"></div>
+        <p style="padding-right: 8px">Check Out Our Catalogs Here!</p>
+        <svg class="point-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+          <path
+            d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
+        </svg>
+      </div>
+    </section>
+
+    <section class="catalogs">
+      <a class="option2" href="./download/24-25 Riveli Catalog Priced.pdf">
+        <p>Riveli Catalog</p>
+      </a>
+      <a class="option2" href="./download/24-25 UC Catalog Priced.pdf">
+        <p>Product Catalog</p>
+      </a>
+      <a class="option2" href="./download/24-25 Sports Catalog.pdf">
+        <p>Sports Catalog</p>
+      </a>
+    </section>
+
+    <section class="bkg-town">
+      <a class="option option3" href="https://backgroundtown.com/">
+        <img src="./img/cart_v1_gray.png" alt="">
+        <p>Shop NOW at BackgroundTown!</p>
+        <div></div>
+      </a>
+    </section>
+
+    <section class="backgroundtown-section">
+      <p>Shop thousands of beautiful backdrop designs printed on demand on your choice of UltraCloth or Fleece. In need
+        of Rubbermat Flooring? We print those too! Simply can't find what you're looking for, then upload your very own
+        custom backdrop!</p>
+      <a href="https://backgroundtown.com/">
+        <img src="./img/backgroundtown-logo.png" alt="">
+      </a>
+    </section>
+
+    <section>
+      <div class="option">
+        <div></div>
+        <p>Follow Us on Social Media</p>
+        <div></div>
+      </div>
+    </section>
+
+    <section class="socials">
+      <a href="https://www.facebook.com/acilab">
+        <img src="./img/_facebook.png" alt="">
+      </a>
+      <a href="#">
+        <img src="./img/_youtube.png" alt="">
+      </a>
+      <a href="https://www.instagram.com/acilab/">
+        <img src="./img/instagram2.png" alt="">
+      </a>
+    </section>
+    <p class="download-btn" id="download-btn">download the app</p>
+  </div>
+
+  <div id="modal" class="modal">
+    <div class="modal-content">
+      <img src="./img/logo.jpg" alt="">
+      <div>
+        <h3>Install this web app on your phone</h3>
+        <p>tap
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" enable-background="new 0 0 50 50">
+            <path d="M30.3 13.7L25 8.4l-5.3 5.3-1.4-1.4L25 5.6l6.7 6.7z" />
+            <path d="M24 7h2v21h-2z" />
+            <path
+              d="M35 40H15c-1.7 0-3-1.3-3-3V19c0-1.7 1.3-3 3-3h7v2h-7c-.6 0-1 .4-1 1v18c0 .6.4 1 1 1h20c.6 0 1-.4 1-1V19c0-.6-.4-1-1-1h-7v-2h7c1.7 0 3 1.3 3 3v18c0 1.7-1.3 3-3 3z" />
+          </svg>
+          then "add to homescreen"
+        </p>
+      </div>
+    </div>
+  </div>
+</body>
+
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('modal');
+    const btn = document.getElementById('download-btn');
+
+    modal.onclick = function() {
+      modal.style.display = 'none';        
+    }
+
+    btn.onclick = function() {
+      modal.style.display = 'block';
+    }
+  });
+</script>
+
+</html>
